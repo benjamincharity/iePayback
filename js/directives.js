c880535a93ddb5df1a1ad98a012d36dc70638689
Benjamin Charity <ben@benjamincharity.com>
2014-03-24 15:01:04 -0400

    minor style tweaks

--- js/directives.js
+++ js/directives.js
@@ -1,7 +1,6 @@
 angular.module('iePayback').directive('selecter', function($timeout) {
 
   var linkFunction = function($scope, $element, $attributes) {
-    console.log($scope.defaultLabel);
     $timeout(function() {
       $element.selecter({
         //label: defaultLabel
